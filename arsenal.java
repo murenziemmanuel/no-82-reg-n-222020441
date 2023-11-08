c71e8b0310e03d7f23aa16d50a52b3a83f756535
murenziemmanuel <149683618+murenziemmanuel@users.noreply.github.com>
2023-11-08 11:14:10 +0200

    Add files via upload

---
+++ arsenal.java
@@ -0,0 +1,7 @@
+//this program will desplay the word arsenal
+public class arsenal {//main class
+    
+    public static void main(String[]args){//main function
+        System.out.println("arsenal");//output
+    }
+}
---
+++ swichcase2.java
@@ -0,0 +1,17 @@
+//this program will display different word according to the switch
+public class swichcase2 {//main class
+    public static void main(String[] args) {//main function
+        int age=9;//initialization
+        switch (age) {//switch
+            case 9://case
+                System.out.println("nine");//output
+                break;//break statement
+            case 8://case
+                System.out.println("eight");//output
+                break; //break statement   
+            default://default
+                System.out.println("false");//output
+                break;//break statement
+        }
+    }
+}
